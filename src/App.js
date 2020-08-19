dc3e87de3456543fa4323e1925e432974811876b
Yuri Godke <yurigodke@gmail.com>
2020-08-19 00:16:42 -0300

    Remove popup

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import classNames from "classnames";
 import style from "./App.module.scss";
 import { Line, Bar } from "react-chartjs-2";
 
-import {
-  AbsoluteNumbers,
-  ChartBox,
-  Footer,
-  Maintenance,
-  Popup
-} from "./modules";
+import { AbsoluteNumbers, ChartBox, Footer, Maintenance } from "./modules";
 
 import prepareData from "./prepareData.js";
 
@@ -59,7 +53,6 @@ class App extends PureComponent {
       if (dataType === "raw") {
         content = (
           <>
-            <Popup />
             <AbsoluteNumbers data={dataConsolidation} />
             {navigationGroup}
             <ChartBox title="Casos e óbitos diários">
